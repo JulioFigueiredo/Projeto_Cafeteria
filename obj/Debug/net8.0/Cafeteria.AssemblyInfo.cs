8fbe0c0e73c04a5cfee0309987d2345d6f0142fe
JulioFigueiredo <julioo.1amorim@gmail.com>
2024-12-10 16:30:57 -0300

    docs: update code

--- obj/Debug/net8.0/Cafeteria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Cafeteria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cebf632bbe757b642fde35e15bdb47c82375def7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2af5d74fc0a18d161b3ae6fd5249e7bc32d67a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
